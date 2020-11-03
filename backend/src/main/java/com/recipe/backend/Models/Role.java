0ea4631e3a06523ce7c385a6d0a27ceb23e45bb1
jbernett <jbernettstl@gmail.com>
2020-11-03 10:21:15 -0600

    added the rest of auth with jwt

--- backend/src/main/java/com/recipe/backend/Models/Role.java
+++ backend/src/main/java/com/recipe/backend/Models/Role.java
@@ -1,21 +1,42 @@
 package com.recipe.backend.Models;
 
 import javax.persistence.*;
+
 import com.recipe.backend.Models.Enums.ERole;
 
-import lombok.NoArgsConstructor;
-
 @Entity
-@NoArgsConstructor
 @Table(name = "ROLES")
 public class Role {
 	@Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "ID")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "ID")
 	private Integer id;
 
 	@Enumerated(EnumType.STRING)
 	@Column(name = "NAME", length = 20)
 	private ERole name;
 
+	public Role() {
+
+	}
+
+	public Role(ERole name) {
+		this.name = name;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public ERole getName() {
+		return name;
+	}
+
+	public void setName(ERole name) {
+		this.name = name;
+	}
 }
--- backend/src/main/java/com/recipe/backend/Models/User.java
+++ backend/src/main/java/com/recipe/backend/Models/User.java
@@ -17,22 +17,22 @@ import javax.validation.constraints.Size;
 public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "ID")
 	private Long id;
 
 	@NotBlank
-    @Size(max = 20)
-    @Column(name = "USERNAME")
+	@Size(max = 20)
+	@Column(name = "USERNAME")
 	private String username;
 
 	@NotBlank
-    @Size(max = 50)
-    @Column(name = "EMAIL")
+	@Size(max = 50)
 	@Email
+	@Column(name = "EMAIL")
 	private String email;
 
 	@NotBlank
-    @Size(max = 120)
-    @Column(name = "PASSWORD")
+	@Size(max = 120)
 	private String password;
 
 	@ManyToMany(fetch = FetchType.LAZY)
@@ -48,5 +48,45 @@ public class User {
 		this.username = username;
 		this.email = email;
 		this.password = password;
-    }
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Set<Role> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(Set<Role> roles) {
+		this.roles = roles;
+	}
 }
